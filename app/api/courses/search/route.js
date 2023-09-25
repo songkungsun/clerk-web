ceecb4b7912a7a4037de5451ec16daa534fe99da
songkungsun <thdrudtjssla1234@gmail.com>
2023-09-25 12:00:17 +0900

    2nd

--- app/api/courses/search/route.js
+++ app/api/courses/search/route.js
@@ -2,8 +2,6 @@ import { NextResponse } from 'next/server'
 import courses from '../data.json'
 export async function GET(request) {
 const { searchParams } = new URL(request.url)
-// console.log(searchParams)
-// console.log(searchParams.get('query'))
 const query = searchParams.get('query')
 const filteredCourses = courses.filter((course) => {
 return course.title.toLowerCase().includes(query.toLowerCase())
--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 export default function Home() {
 return (
 <>
-<h1>Home</h1>
+<h1>안녕하세요</h1>
 </>
 )
 }
