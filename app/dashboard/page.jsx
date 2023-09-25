d15fad69d92401ec02d54d493ab09a3c5a8e2e2d
songkungsun <thdrudtjssla1234@gmail.com>
2023-09-25 11:59:42 +0900

    2nd

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -7,8 +7,7 @@ const DashboardPage = () => {
     <div>
       <h1 className="text-2xl font-bold mb-5">Dashboard</h1>
       <p className="mb-5">
-        Welcome to Dashboard. This page shows the personal information of the
-        logged-in user.
+        HI, my name is Songkungsun
       </p>
       <p>userId : {userId}</p>
     </div>
